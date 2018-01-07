6ae62e8983c7953354d304415e7dab98e2be3364
Bui Chi Kien <kiencbui@gmail.com>
2018-01-07 13:36:34 +0700

    post location to database

---
+++ Grab-Location-Management/assests/taixe-app.js
@@ -0,0 +1,56 @@
+var currentLocation = { lat: 10.7626737, lng: 106.6834609 };
+var currentCarKey = "-L2EDUEvdfWlklHoJdls";
+const GRABCAR = "GrabCars";
+
+$(function () {
+
+    var interval = setInterval(function(){
+        console.log(currentLocation.lat + '  ' + currentLocation.lng);
+        updateCarLocation(currentCarKey,currentLocation.lat,currentLocation.lng);
+    }, 5000);
+
+})
+
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        center: currentLocation
+    });
+
+    geocoder = new google.maps.Geocoder;
+    directionsService = new google.maps.DirectionsService;
+    directionsDisplay = new google.maps.DirectionsRenderer; 
+    directionsDisplay.suppressMarkers = false;
+
+    google.maps.event.addListener(map, "click", function (e) {
+
+        currentLocation = { lat: e.latLng.lat(), lng: e.latLng.lng()};
+        // alert(currentLocation);
+
+    });
+
+    directionsDisplay.setMap(map);
+    directionsDisplay.setOptions( { suppressMarkers: true } );
+}
+
+// Initialize Firebase
+var firebaseConfig = {
+    apiKey: "AIzaSyDh7WAhJnaIcpUEGiPaMQrzOjQwuzWw1RA",
+    authDomain: "my-grab.firebaseapp.com",
+    databaseURL: "https://my-grab.firebaseio.com",
+    projectId: "my-grab",
+    storageBucket: "my-grab.appspot.com",
+    messagingSenderId: "155666237539"
+};
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
+var grabCarsRef = database.ref(GRABCAR);
+
+
+function updateCarLocation(carKey, lat, lng) {
+    let path = GRABCAR +'/'+ carKey;
+    database.ref(path).update({
+        latitude: lat,
+        longitude: lng
+    })
+}
